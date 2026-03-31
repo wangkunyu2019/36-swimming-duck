cb6ac164f1e6cde70e6212fe2efc7b9da234b0b5
wangkunyu2019 <2019040262@qq.com>
2026-03-31 22:10:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 9; index++) {
+        basic.showIcon(IconNames.Duck)
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            # # # . .
+            . # # # #
+            . # # # .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 9; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # # . #
+            # . # # .
+            . # # . #
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . . .
+            # # . # .
+            . # # . .
+            # # . # .
+            . # . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            # . # . .
+            # # . . .
+            # . # . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
 })
